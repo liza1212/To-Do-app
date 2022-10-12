fd1b4ce4d2d44d59f7df81c981062b0d61fbb94c
Liza Shrestha <lisashr12@gmail.com>
2022-10-12 20:13:14 +0545

    Day 2, Used Express to add edit, delete and add new to-do features

---
+++ resource/todo.js
@@ -0,0 +1,24 @@
+const todos=[
+    {
+        "_id":"000",
+        "title":"Complete bootcamp project",
+        "description":"Talk to team, make team repository, assign task"
+    },
+    {
+        "_id":"001",
+        "title":"Bake",
+        "description":"Buy coco powder, baking powder, flour, eggs"
+    },
+    {
+        "_id":"002",
+        "title":"Complete Assignment",
+        "description":"Check google classroom, surf internet, buy notebooks"
+    },
+    {
+        "_id":"003",
+        "title":"Workout",
+        "description":"Wake up early, warm up exercise, squats, jumping jacks"
+    }
+]
+
+module.exports = todos; //when exporting with same name 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,60 @@
 var express = require('express');
 var router = express.Router();
+var todos= require('../resource/todo')
+// here one dot mean it will search for files inside current folder(inside routes), double dot means
+// it goes one step outside(inside day4_expree) and if three dots, it will further move out
+// console.log(todos);
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { name: 'Liza Shrestha',age:'21', sem:'sixth' });
+  // res.render('index', { name: 'Liza Shrestha',age:'21', sem:'sixth' });
+    res.render('home', {todosList:todos});
 });
 
-router.get('/home', function(req, res, next){
-  res.render('home');
+// router.get('/home', function(req, res, next){
+//   res.render('home');
+// });
+
+router.get('/add-to-do', function(req, res, next){
+  res.render('addToDo', {title:"Add to do"});
+});
+
+router.post('/save-to-do', function(req, res, next){
+  todos.push({...req.body, _id:`00${todos.length}`});
+  res.redirect('/');
+});
+
+//'/delete-tot-do/:index' here :/ used for dynamic values, similar to jinja
+// router.get ('/delete-to-do/:index', function(req, res, next){
+//   console.log(req.params.index)
+//   todos.splice(req.params.index, 1); //to remove the element at that index
+//   res.redirect('/');
+// });
+
+router.get ('/delete-to-do/:id', function(req, res, next){
+  console.log(req.params.id)
+  todos.splice(req.params.id, 1); //to remove the element at that index
+  res.redirect('/');
+});
+
+// router.get ('/delete-to-do/:index', function(req, res, next){
+//   console.log(req.params.index)
+//   todos.splice(req.params.index, 1); //to remove the element at that index
+//   res.redirect('/');
+// });
+
+router.post('/edit-to-do/:id', function (req, res, next){
+  console.log("Hello")
+  console.log(req.params);
+  console.log(req.body);
+  console.log("Biee");
+  const index=todos.findIndex(todo=>todo._id===req.params.id);
+  todos.splice(index, 1, {...req.body, _id:req.params.id});
+  res.redirect('/');
+});
+
+router.get('/open-update-form/:id', function(req, res, next){
+  const todotodo = todos.find(todo=>todo._id===req.params.id)
+  res.render('editToDo', {todo:todotodo});
 });
 
 module.exports = router;
